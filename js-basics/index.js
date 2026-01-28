32a2f5a05a777235a965b4d93586a49d1f6b4302
Abhikansh Mittal <abhikansh33@gmail.com>
2026-01-28 17:25:32 +0530

    q2 & 3 done

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,18 +1,35 @@
 
+//Q1
+// const input = {
+//   food: [10, 20, 30],
+//   travel: [5, 15],
+//   bills: [40, 60]
+// };
+//
+// const result = Object.fromEntries(
+//   Object.entries(input).map(([key, arr]) => [
+//     key,
+//     arr.reduce((sum, val) => sum + val, 0)
+//   ])
+// );
+//
+// console.log(result);
 
-const input = {
-  food: [10, 20, 30],
-  travel: [5, 15],
-  bills: [40, 60]
-};
+// //Q2
+// const input2 = ["apple", "banana", "apple", "orange", "banana", "apple"]
+//
+// const result2 = {}
+//
+// input2.forEach(word => {
+//   result2[word] = (result2[word] || 0) + 1
+// })
+//
+// console.log(result2);
 
-const result = Object.fromEntries(
-  Object.entries(input).map(([key, arr]) => [
-    key,
-    arr.reduce((sum, val) => sum + val, 0)
-  ])
-);
+//Q3
+const input3 = { a: "x", b: "y", c: "z" }
 
-console.log(result);
-
-
+const output3 = Object.fromEntries(
+  Object.entries(input3).map(([key, value]) => [value, key])
+)
+console.log(output3)
